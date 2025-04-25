e412f29acb7d86c3128a9bc5682738ec3d65b48a
Eduardo Sampaio <duardosampaiio456@gmail.com>
2025-04-25 15:46:01 -0300

    feat: Criei middleware de autenticação, e adicionei um efeito de rolagem ao clicar no botão leia mais da homepage.

---
+++ helpers/isAuthenticated.js
@@ -0,0 +1,17 @@
+module.exports = {
+    eAdmin: function (req, res, next) {
+      if (req.isAuthenticated() && req.user.eAdmin === 1) {
+        return next();
+      }
+      req.flash("error_msg", "Você precisa ser um administrador.");
+      res.redirect("/");
+    },
+    estaLogado: function (req, res, next) {
+      if (req.isAuthenticated()) {
+        return next();
+      }
+      req.flash("error_msg", "Você precisa estar logado para ver esta página.");
+      res.redirect("/usuarios/login");
+    }
+  };
+  
